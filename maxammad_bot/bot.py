3c114cce08f925d562833021acc4ff2a155e173b
anviks <anviks@ttu.ee>
2023-02-20 11:05:44 +0200

    added max quote database.

--- maxammad_bot/bot.py
+++ maxammad_bot/bot.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import random
 
 from discord.ext import commands
 from discord.utils import get
@@ -10,8 +11,13 @@ import responses
 async def send_message(message, user_message, is_private):
     try:
         response: str = responses.handle_response(user_message)
+        if is_private is None:
+            with open("max_quotes.txt", encoding="utf-8") as f:
+                response = random.choice(f.read().split("\n"))
         if "&ping&" in response:
             response = response.replace("&ping&", message.author.mention)
+        if "&name&" in response:
+            response = response.replace("&name&", message.author)
         if "&reply&" in response:
             response = response.strip("&reply&")
             await discord.Message.reply(message, response)
@@ -32,6 +38,7 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f"{client.user} is now running!")
+        # await client.get_guild(1011692357806198784).get_channel(1011698709215588372).send("Hello, I'm back")
 
     @client.event
     async def on_message(message):
@@ -40,14 +47,21 @@ def run_discord_bot():
 
         username = str(message.author)
         user_message = str(message.content)
-        channel = str(message.channel)
+        global channel
+        channel = message.channel
 
-        print(f"[{datetime.datetime.now()}] {username} said: '{user_message}' in #{channel}.")
+        print(f"[{datetime.datetime.now()}] {username} said: '{user_message}' in #{str(channel)}.")
 
+        if client.user.mentioned_in(message):
+            await send_message(message, user_message, is_private=None)
         if user_message[0] == "%":
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
         else:
             await send_message(message, user_message, is_private=False)
+
+    @client.event
+    async def on_disconnect():
+        await client.get_guild(1011692357806198784).get_channel(1011698709215588372).send("Good night!")
 
     client.run(muh_token)
--- maxammad_bot/responses.py
+++ maxammad_bot/responses.py
@@ -9,6 +9,11 @@ def handle_response(message: str) -> str:
     if "vaband" in message:
         return "Väga väga palun vabandust ma rohkem selles ära kirjuta ma luban sind."
 
+    if "diskmat" in message:
+        return "Tere! Selle aine mul on kõik korras ja ma läbitud eelmisel semestril diskreetne " \
+               "matemaatika aine Harri Lensile ja mul on hinne 1 kõik korras läbitud kursuse. " \
+               "Kellele on vaja abi kirjutada mulle privaat sõnume."
+
     if "max" in message:
         return "Hello, may question?"
 
@@ -18,6 +23,11 @@ def handle_response(message: str) -> str:
     if "ära " in message or "Ära " in message:
         return "hästi ei teha rohkem vabandust palun"
 
+    if "deklareer" in message:
+        return "Tere,kõigile ma selle aine deklareerida sellel semestril. Mul on selline küsimus, " \
+               "mis on vaja selle aine teha missuguseid ülesandeid kodune töö või mida? " \
+               "Ma ei saa aru natuke selles aines. Ja arvestustöö toimub ülikoolis või kodus? Vabandust, palun kõigist."
+
     if "aine" in message or "kursus" in message:
         return random.choice(["Ma eelmisel aastal ära deklareeritud selle hulgad ained. "
                               "Ma deklareerisin sellel semestril,vabandust palun. "
@@ -25,16 +35,13 @@ def handle_response(message: str) -> str:
                               "semestril slaidide lingis ja õppida pähe ka, sest et varsti on esimene "
                               "kontrolltöö ja on vaja teen suurepärane ja viskada miinimumi punkti või rohkem, "
                               "kui ma saan.",
-                              "Tere! Selle aine mul on kõik korras ja ma läbitud eelmisel semestril diskreetne matemaatika aine Harri Lensile ja mul on hinne 1 kõik korras läbitud kursuse. Kellele on vaja abi kirjutada mulle privaat sõnume.",
+                              "See aine ma juba läbitud eelmisel aastal sügisel ja mulle hinne on 1. "
+                              "Mul on korras, ma saan abistada kui on abi?",
                               "Tere vabandust mis see aine on? &ping&"])
 
     if "avatud ülikool" in message:
         return "Saan aru, aga ma õpin Avatud Ülikooli noh gruppis IADB jah see on õgige vabandust &ping&"
 
-    if "deklareer" in message:
-        return "Tere,kõigile ma selle aine deklareerida sellel semestril. Mul on selline küsimus, " \
-               "mis on vaja selle aine teha missuguseid ülesandeid kodune töö või mida? " \
-               "Ma ei saa aru natuke selles aines. Ja arvestustöö toimub ülikoolis või kodus? Vabandust, palun kõigist."
-
     if "privaat" in message or "sõber" in message:
-        return random.choice(["Privaatsõnume? Vabandust.", "Tere! Selle aine mul on kõik korras ja ma läbitud eelmisel semestril diskreetne matemaatika aine Harri Lensile ja mul on hinne 1 kõik korras läbitud kursuse. Kellele on vaja abi kirjutada mulle privaat sõnume."])
+        return random.choice(["Privaatsõnume? Vabandust.",
+                              "Tere! Selle aine mul on kõik korras ja ma läbitud eelmisel semestril diskreetne matemaatika aine Harri Lensile ja mul on hinne 1 kõik korras läbitud kursuse. Kellele on vaja abi kirjutada mulle privaat sõnume."])
